fb2451eafe159ab15aa64206c7777325bfb0447a
Milan Cvetkovic <milan.cvetkovic.807@gmail.com>
2026-01-02 18:32:55 +0100

    De-structuring properites

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import './App.css'
 
 function App() {
 
-    function Header(props) {
-        // console.log(props);
+    function Header({ name, year }) {
         return (
             <header>
-                <h1>{props.name}'s Kitchen</h1>
-                <p>Copyright {props.year}</p>
+                <h1>{name}'s Kitchen</h1>
+                <p>Copyright {year}</p>
             </header>
         )
     }
