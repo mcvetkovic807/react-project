35c72728996e01bd85b9c7d83151fd649543ed4b
Milan Cvetkovic <milan.cvetkovic.807@gmail.com>
2026-01-04 18:52:30 +0100

    04_01b Array desctucturing

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const animals = ["giraffe", "zebra", "bear"];
+console.log(animals[0]);
+
+// const [firstAnimal, second, third] = ["giraffe", "zebra", "bear"];
+// console.log(firstAnimal);
+
+const [,, third] = ["giraffe", "zebra", "bear"];
+console.log(third);
+
 createRoot(document.getElementById('root')).render(<App />)
